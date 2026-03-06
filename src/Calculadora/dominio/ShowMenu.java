0dc3278800d8ed58b8a4379e831631b4a14b9f04
Bruno Lopes Borges <brunolopesborges2007@gmail.com>
2026-03-06 12:53:18 -0300

    Calculadora

---
+++ src/Calculadora/dominio/ShowMenu.java
@@ -0,0 +1,12 @@
+package Calculadora.dominio;
+
+public class ShowMenu {
+    public void menu1(){
+        System.out.println("ESCOLHA A OPCAO QUE DESEJA FAZER");
+        System.out.println("[1] Somar");
+        System.out.println("[2] Subtrair");
+        System.out.println("[3] Multiplicar");
+        System.out.println("[4] Dividir");
+        System.out.println("--------------------------------------");
+    }
+}
--- src/Calculadora/test/Main.java
+++ src/Calculadora/test/Main.java
@@ -1,23 +1,24 @@
 package Calculadora.test;
 
 import Calculadora.dominio.MathOperation;
+import Calculadora.dominio.ShowMenu;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        MathOperation operation = new MathOperation();
+        ShowMenu menu = new ShowMenu();
         String again = "S";
         while (again.equalsIgnoreCase("S")) {
-
-            System.out.println("ESCOLHA A OPCAO QUE DESEJA FAZER");
-            System.out.println("[1] Somar");
-            System.out.println("[2] Subtrair");
-            System.out.println("[3] Multiplicar");
-            System.out.println("[4] Dividir");
-            System.out.println("--------------------------------------");
+            menu.menu1();
 
             int option = scanner.nextInt();
+            if (option < 1 || option > 4){
+                System.out.println("Opcao invalida");
+                continue;
+            }
 
             double num1;
             double num2;
@@ -27,7 +28,6 @@ public class Main {
             System.out.println("Qual o segundo número? ");
             num2 = scanner.nextDouble();
 
-            MathOperation operation = new MathOperation();
             switch (option) {
                 case 1:
                     System.out.println("O resultado é: " + operation.addNumbers(num1, num2));
@@ -36,16 +36,21 @@ public class Main {
                     System.out.println("O resultado é: " + operation.subtractNumbers(num1, num2));
                     break;
                 case 3:
-                    System.out.println("O resultado é: " + operation.multiplicationNumbers(num1, num2));
+                        System.out.println("O resultado é: " + operation.multiplicationNumbers(num1, num2));
                     break;
                 case 4:
-                    System.out.println("O resultado é: " + operation.divisionNumbers(num1, num2));
+                    if (num2 == 0){
+                        System.out.println("Nao existe divisao por zero!");
+                    }else {
+                        System.out.println("O resultado é: " + operation.divisionNumbers(num1, num2));
+                    }
                     break;
+
             }
             System.out.println("Deseja fazer outra operação?[S/N]");
             again = scanner.next();
             if (again.equalsIgnoreCase("N")){
-                System.out.println("Calculadora' encerrada");
+                System.out.println("Calculadora encerrada.");
             }
         }
         scanner.close();
